a27dfb95c72390ed68622e8f016bc19422971f4e
vietdh <vietdanghuy.95@gmail.com>
2023-12-10 23:51:46 +0700

    Apply navbar in all templates

--- food/urls.py
+++ food/urls.py
@@ -5,7 +5,7 @@ app_name = 'food'
  
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('/<int:item_id>', views.detail, name='detail'),
+    path('<int:item_id>', views.detail, name='detail'),
     path('/item', views.item, name='item'),
     path('', views.index, name='index'),
 ]
