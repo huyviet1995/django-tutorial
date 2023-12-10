972610c1ddd34f4d85cfe04ebf6ef8ca4798b70a
vietdh <vietdanghuy.95@gmail.com>
2023-12-10 23:37:26 +0700

    Add navbar

--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,11 @@
 from . import views
 from django.urls import path
+
+app_name = 'food'
  
 urlpatterns = [
-    path('', views.index, name='index'),
     # path('admin/', admin.site.urls),
     path('/<int:item_id>', views.detail, name='detail'),
     path('/item', views.item, name='item'),
+    path('', views.index, name='index'),
 ]
